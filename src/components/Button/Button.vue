48c4d12db488e9218bb296f9d4ef92c74f804a13
rizqi-reza <rizqireza.valhevi@gmail.com>
2022-09-06 16:10:46 +0700

    Fixing button style

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
-import { reactive } from 'vue';
-import Color, { TColor } from '../../tokens/color';
+import { reactive, computed } from 'vue';
+import Color, { EColor, TColor } from '../../tokens/color';
 import { Height, FontSize, TSize } from '../../tokens/size';
 import { TVariant } from '../../tokens/variant';
 
@@ -22,40 +22,46 @@ const emit = defineEmits(['click']);
 const onClick = emit('click');
 
 const colorStyle = Color[color];
-const baseClass =
-  'flex items-center px-3 py-1 text-white font-bold cursor-pointer opacity-90 rounded-md';
 const sizeClass = reactive({
   [Height[size]]: true,
 });
-const backgroundClass = reactive({
-  [`bg-${colorStyle}`]: isSolid,
-});
-const borderClass = reactive({
-  [`border-2 border-${colorStyle}`]: isOutline,
-});
-const textClass = reactive({
-  [FontSize[size]]: true,
-  [`text-${colorStyle}`]: !isSolid,
-  'text-black': isSolid && color === 'warning',
+const backgroundClass = computed(() => ({
+  [`bg-${colorStyle}`]: isSolid && !!colorStyle,
+  // 'bg-blue-700': isSolid && color === EColor.PRIMARY,
+  // 'bg-green-600': isSolid && color === EColor.SUCCESS,
+  // 'bg-yellow-300': isSolid && color === EColor.WARNING,
+  // 'bg-red-700': isSolid && color === EColor.DANGER,
+  // 'bg-blue-400': isSolid && color === EColor.INFO,
+}));
+const borderClass = computed(() => ({
+  [`border-2 border-${colorStyle}`]: isOutline && !!colorStyle,
+  // 'border-2 border-blue-700': isOutline && color === EColor.PRIMARY,
+  // 'border-2 border-green-600': isOutline && color === EColor.SUCCESS,
+  // 'border-2 border-yellow-300': isOutline && color === EColor.WARNING,
+  // 'border-2 border-red-700': isOutline && color === EColor.DANGER,
+  // 'border-2 border-blue-400': isOutline && color === EColor.INFO,
+}));
+const textClass = computed(() => ({
+  [FontSize[size] as string]: true,
+  [`text-${colorStyle}`]: !isSolid && !!colorStyle,
+  'text-white': isSolid && color !== EColor.WARNING,
   'hover:underline': variant === 'link',
   'hover:opacity-100': !disabled,
-});
+}));
 const disabledClass = reactive({
   'opacity-50': disabled,
-  'cursor-not-allowed': disabled,
+  'cursor-not-allowed': disabled
 });
-console.log(colorStyle)
 </script>
 
 <template>
-  <button type="button" :class="[
-    baseClass,
+  <button type="button" class="flex items-center px-3 py-1 font-bold cursor-pointer opacity-90 rounded-md" :class="[
     sizeClass,
     backgroundClass,
     borderClass,
     textClass,
     disabledClass,
-  ]" @click="onClick">
+  ]" @click="onClick" :disabled="disabled">
     <svg v-if="loading" class="animate-spin -ml-1 mr-3 h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none"
       viewBox="0 0 24 24">
       <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>

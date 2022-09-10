f396f663becfa363e0674c91f46628dc43ed0c44
rizqi-reza <rizqireza.valhevi@gmail.com>
2022-09-11 00:19:42 +0700

    Fixing build css module export

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue';
 import dts from 'vite-plugin-dts'
-import libCss from 'vite-plugin-libcss';
 const resolveAlias = require('./tsconfig-alias');
 
 // https://vitejs.dev/config/
@@ -12,7 +11,6 @@ export default defineConfig({
     dts({
       insertTypesEntry: true,
     }),
-    libCss(),
   ],
   resolve: {
     alias: resolveAlias(),
